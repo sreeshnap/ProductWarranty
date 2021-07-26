4dc76268474b2e44e4a9d96949f417c528c08cdd
sreeshnap <sreeshna.p.vijayan@gmail.com>
2021-07-26 17:42:08 +0530

    [ADD] Initial commit

--- myaddons/website_visibility/models/allowed_products_customer.py
+++ myaddons/website_visibility/models/allowed_products_customer.py
@@ -3,21 +3,18 @@ from odoo import api, fields, models
 class AllowedProductInherit(models.Model):
     _inherit = 'res.partner'
 
-    allowed_products_ids = fields.One2many('customer.product', 'product_id', 'same_vat_partner_id')
+
+    allowed_products_ids = fields.Many2many('product.template', domain="[('is_published', '=', True)]")
 
 
-class AllowedProduct(models.Model):
-    _name = 'customer.product'
-
-    product_id = fields.Many2one('product.product')
-    same_vat_partner_id = fields.Many2one('res.partner')
+    # website_available_product_ids = fields.Many2many('product.template', string='Available Product',
+    #                                                  domain="[('is_published', '=', True)]")
+    # website_available_cat_ids = fields.Many2many('product.public.category', string='Available Product Categories')
 
 
-    # @api.depends('same_vat_partner_id')
-    # def _compute_product_id(self):
-    #     self.allowed_products.write({
-    #         'allowed_products_ids': [(1, 0, {
-    #             'product_id': self.product_id,
-    #         })]
-    #     })
+class WebsiteGuestVisibility(models.TransientModel):
+    _inherit = 'res.config.settings'
+
+    available_product_ids = fields.Many2many('product.template', string='Available Product',
+                                             domain="[('is_published', '=', True)]")
 
