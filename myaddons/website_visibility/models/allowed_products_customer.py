a774481f47442e0fc881202c0127f34e8ef57dbc
sreeshnap <sreeshna.p.vijayan@gmail.com>
2021-07-27 20:25:54 +0530

    [ADD] Initial commit

--- myaddons/website_visibility/models/allowed_products_customer.py
+++ myaddons/website_visibility/models/allowed_products_customer.py
@@ -2,19 +2,19 @@ from odoo import api, fields, models
 
 class AllowedProductInherit(models.Model):
     _inherit = 'res.partner'
+    # _name = 'allowed.product'
+
+    filter_mode = fields.Selection([('null', 'No Filter'), ('product_only', 'Product Wise'),
+                                    ('categ_only', 'Category Wise')], string='Filter Mode', default='null')
+    website_available_product_ids = fields.Many2many('product.template', string='Available Product',
+                                                     domain="[('is_published', '=', True)]")
+    website_available_cat_ids = fields.Many2many('product.public.category', string='Available Product Categories')
+
+    @api.onchange("filter_mode")
+    def onchange_filter_mod(self):
+        if self.filter_mode == 'null':
+            self.website_available_cat_ids = None
+            self.website_available_product_ids = None
 
 
-    allowed_products_ids = fields.Many2many('product.template', domain="[('is_published', '=', True)]")
-
-
-    # website_available_product_ids = fields.Many2many('product.template', string='Available Product',
-    #                                                  domain="[('is_published', '=', True)]")
-    # website_available_cat_ids = fields.Many2many('product.public.category', string='Available Product Categories')
-
-
-class WebsiteGuestVisibility(models.TransientModel):
-    _inherit = 'res.config.settings'
-
-    available_product_ids = fields.Many2many('product.template', string='Available Product',
-                                             domain="[('is_published', '=', True)]")
 
--- myaddons/website_visibility/models/user_login_allowed_products.py
+++ myaddons/website_visibility/models/user_login_allowed_products.py
@@ -1,8 +1,11 @@
 from odoo import http
 from odoo.addons.website_sale.controllers.main import WebsiteSale
+from odoo import fields, models, api
+from ast import literal_eval
 
 
 class RelatedUserLogin(WebsiteSale):
+    _inherit = 'res.partner'
 
     @http.route([
         '''/shop''',
@@ -13,3 +16,43 @@ class RelatedUserLogin(WebsiteSale):
     def shop(self, page=0, category=None, search='', ppg=False, **post):
         res = super(RelatedUserLogin, self).shop(page=0, category=None, search='', ppg=False, **post)
         return res
+
+    @api.model
+    def set_values(self):
+        res = super(RelatedUserLogin, self).set_values()
+        self.env['ir.config_parameter'].sudo().set_param('user', self.user)
+        self.env['ir.config_parameter'].sudo().set_param('filter_mode', self.filter_mode)
+        if not self.user:
+            self.website_available_cat_ids = None
+            self.website_available_product_ids = None
+            self.env['ir.config_parameter'].sudo().set_param('filter_mode', 'product_only')
+        if self.filter_mode == 'product_only':
+            self.website_available_cat_ids = None
+        elif self.filter_mode == 'categ_only':
+            self.website_available_product_ids = None
+
+        self.env['ir.config_parameter'].sudo().set_param('website_product_visibility.website_available_product_ids',
+                                                         self.website_available_product_ids.ids)
+        self.env['ir.config_parameter'].sudo().set_param('website_product_visibility.website_available_cat_ids',
+                                                         self.website_available_cat_ids.ids)
+        return res
+
+    @api.model
+    def get_values(self):
+        res = super(RelatedUserLogin, self).get_values()
+        product_ids = literal_eval(
+            self.env['ir.config_parameter'].sudo().get_param('website_product_visibility.website_available_product_ids',
+                                                             'False'))
+        cat_ids = literal_eval(
+            self.env['ir.config_parameter'].sudo().get_param('website_product_visibility.website_available_cat_ids', 'False'))
+        mod = self.env['ir.config_parameter'].sudo().get_param('filter_mode')
+        res.update(
+            user=self.env['ir.config_parameter'].sudo().get_param(
+                'user'),
+            filter_mode=mod if mod else 'product_only',
+            website_available_product_ids=[(6, 0, product_ids)],
+            website_available_cat_ids=[(6, 0, cat_ids)],
+        )
+        return res
+
+
